796cbb06b92c6e14e274699d369c3e55b9a56f9b
DreiII664 <diegocremonini664@gmail.com>
2025-06-11 23:01:45 -0300

    att1.1

--- js/firebase_info.js
+++ js/firebase_info.js
@@ -6,7 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {};
+const firebaseConfig = {erro né};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
